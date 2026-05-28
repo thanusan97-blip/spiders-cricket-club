eae5152f47ad6fe3894c20c38d0fea7e0d965dc8
thanusan97 <thanusan97@gmail.com>
2026-05-28 12:29:47 +0100

    Fix VCTB 2025 gallery route

---
+++ app/gallery/btcl/2025/page.tsx
@@ -0,0 +1,7 @@
+export default function BTCL2024Gallery() {
+  return (
+    <main className="min-h-screen bg-[#eef2ff] px-6 py-20 text-[#071a52]">
+      <h1 className="text-5xl font-black">BTCL 2024</h1>
+    </main>
+  );
+}
