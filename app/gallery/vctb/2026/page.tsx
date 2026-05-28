7c0f520028888fa77004b4a2e9c769e0ceda0662
thanusan97 <thanusan97@gmail.com>
2026-05-28 13:49:41 +0100

    Updated VCTB 2026 page

--- app/gallery/vctb/2026/page.tsx
+++ app/gallery/vctb/2026/page.tsx
@@ -1,7 +1,7 @@
 export default function BTCL2024Gallery() {
   return (
     <main className="min-h-screen bg-[#eef2ff] px-6 py-20 text-[#071a52]">
-      <h1 className="text-5xl font-black">BTCL 2024</h1>
+      <h1 className="text-5xl font-black">VCTB 2026 Coming soon....</h1>
     </main>
   );
 }
